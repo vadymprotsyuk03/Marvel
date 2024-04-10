eac34a1214cda47b7b6b2320481f3d39442f56cb
Vadym <vadumprotsyuk@gmail.com>
2024-04-10 20:30:46 +0300

    use componentDidUpdate()

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,13 +2,13 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=84ec864cead8868f50d8705b26b1c990';
-    getResource = async(url) => {
+    getResource = async (url) => {
         let res = await fetch(url);
-
+    
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
@@ -24,11 +24,13 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url 
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
