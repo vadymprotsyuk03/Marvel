3f1d0a055d2846cfa4dfd57a90713c172c1ffad9
Vadym <vadumprotsyuk@gmail.com>
2024-04-14 09:54:07 +0300

    check data with PropTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -107,7 +109,11 @@ class CharList extends Component {
                 </button>
             </div>
         )
+
     }
+}
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
